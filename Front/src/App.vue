86da439dae5c69dffacf837f3bbce3ef625a0644
KGN0826 <rjsska8026@gmail.com>
2025-12-05 16:46:20 +0900

    Vue basic code

---
+++ Front/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>App vue</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Front/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Home vue</h2>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
